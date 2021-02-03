96e833c35f010cb31c8717d3554587f0ab0b9066
step21 <step21@devtal.de>
2021-02-03 13:26:17 +0100

    deploy license contract and added comment for newLicense abi

--- scripts/deploy.py
+++ scripts/deploy.py
@@ -7,3 +7,7 @@ def main():
     """ Simple deploy script for our two contracts. """
     accounts[0].deploy(LicenseContract)
     # LicenseContract[0].newLicense((uint256,bytes32,uint8,uint8,bool,bool,uint256,bool,bool,uint256,uint256) _license, {'from': Account})
+
+
+# (16, )
+# license id, work_hash
