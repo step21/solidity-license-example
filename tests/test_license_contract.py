becbb7be26072479dc55a36d227fe59495c1bfc5
step21 <step21@devtal.de>
2021-02-03 13:24:08 +0100

    removed template tests and test for deployment of licenseContract

---
+++ tests/test_license_contract.py
@@ -0,0 +1,5 @@
+def test_license_contract_deploy(license_contract):
+    """
+    Test if the contract is correctly deployed.
+    """
+    assert license_contract == "0x73B74F5f1d1f7A00d8c33bFbD09744eD90220D12"
