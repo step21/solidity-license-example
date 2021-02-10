a9e3317d7f829144372412a9fb542a4e61b7e13b
step21 <step21@devtal.de>
2021-02-10 17:40:27 +0100

    preliminary ui experiments

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React, {Component} from "react"
 import ReactDOM from "react-dom";
+import { Container, Row, Col } from 'react-grid-system';
 import './App.css'
 import {getWeb3} from "./getWeb3"
 import map from "./artifacts/deployments/map.json"
 import {getEthereum} from "./getEthereum"
 import {
+  MetaMaskButton,
   BaseStyles,
   Heading,
   Flex,
@@ -19,7 +21,9 @@ import {
   Radio,
   Card
 } from "rimble-ui";
-import {ThemeProvider} from 'styled-components'
+import {ThemeProvider} from 'styled-components';
+import NetworkIndicator from '@rimble/network-indicator';
+import ConnectionBanner from '@rimble/connection-banner';
 
 class App extends Component {
 
@@ -55,12 +59,13 @@ class App extends Component {
 
         // Get the current chain id
         const chainid = parseInt(await web3.eth.getChainId())
-
+            
         this.setState({
             web3,
             accounts,
-            chainid
-        }, await this.loadInitialContracts)
+            chainid,
+        }, await this.loadInitialContracts) 
+        
 
     }
 
@@ -132,22 +137,23 @@ class App extends Component {
             licenseContract, license
         } = this.state
 
-        if (!web3) {
-            return <div>Loading Web3, accounts, and contracts...</div>
-        }
+        
 
         if (isNaN(chainid) || chainid <= 42) {
-            return <div>Wrong Network! Switch to your local RPC "Localhost: 8545" in your Web3 provider (e.g. Metamask)</div>
+            console.log("<div>Wrong Network! Switch to your local RPC 'Localhost: 8545' in your Web3 provider (e.g. Metamask)</div>");
         }
 
         if (!licenseContract) {
-            return <div>Could not find a deployed contract. Check console for details.</div>
+            console.log("<div>Could not find a deployed contract. Check console for details.</div>");
         }
 
         const isAccountsUnlocked = accounts ? accounts.length > 0 : false
 
         return (<div className="App">
-            <h1>Your Brownie Mix is installed and ready.</h1>
+            <Container>
+                <Row>
+                <Col>
+            <Heading as="h1">Software License Evaluation Contract.</Heading>
             <p>
                 If your contracts compiled and deployed successfully, you can see the current
                 storage values below.
@@ -159,12 +165,13 @@ class App extends Component {
                     </p>
                     : null
             }
-            <h2>Vyper Storage Contract</h2>
+            <Heading>Vyper Storage Contract</Heading>
 
+            <Box>
             <div>The stored value is: {license}</div>
             <br/>
             <form onSubmit={(e) => this.changeVyper(e)}>
-                <div>
+                
                     <label>Change the value to: </label>
                     <br/>
                     <input
@@ -174,11 +181,53 @@ class App extends Component {
                         onChange={(e) => this.setState({license: e.target.value})}
                     />
                     <br/>
+                    </form>
+                    </Box>
+                    </Col>
+                    <Col>
+                    <NetworkIndicator currentNetwork={null} requiredNetwork={1}>
+  {{
+    onNetworkMessage: "Connected to correct network",
+    noNetworkMessage: "Not connected to anything",
+    onWrongNetworkMessage: "Wrong network"
+  }}
+</NetworkIndicator>
                     <button type="submit" disabled={!isAccountsUnlocked}>Submit</button>
-                </div>
-            </form>
+                <MetaMaskButton.Outline size="small">
+                    Connect with MetaMask
+                    </MetaMaskButton.Outline>
+                    </Col>
+                </Row>
+            <Row>
+    <Col sm={4}>
+      <Box>
+      <Text>
+      <Heading>This is a license agreement for a software evaluation license.</Heading>
 
+<Card>*LICENSOR* - Unnamed Software Company.</Card>
+<Card>*LICENSEE* - Unnamed University or other Organization.</Card>
+*SUBLICENSEE* - A student as set force in the appendix. (array of persons)  
+*ARBITER* - An arbiter or oracle that decides in case of disputes.  Can be a natural or legal person or a machine. 
+Art.  2, 3 and 4 especially are evaluated by the arbiter in case of disputes.  
+*ASSET* - An asset to be licensed.  
 
+* Article 1. The Licensor grants the Licensee  a limited  license  to use  and  evaluate  asset  X  and  grant  sublicenses among group Y, for  use  and  evaluation, in exchange for a licensing fee. This limited license lasts from DD.MM.YYYY (inclusive) to DD.MM.YYYY (exclusive).
+* Article 2. (optional) The (Sub)Licensee is commissioned to publish comments about the  use  of  the  product, in exchange for an evaluation fee. This allows publication of comments about the evaluation of *ASSET*. Furthermore, this requires the publication of comments about the evaluation of the *ASSET*.
+* Article 3. The (Sub)Licensee must not publish comments of the use and evaluation of the Product without the approval of the Licensor; said approval must be obtained before the publication of comments about the evaluation of *ASSET*. If the Licensee publishes results of the evaluation of the Product without approval from the Licensor, the Licensee has 24 h to remove the material from the time of publication. If the time lapses without a removal of the comments of the evaluation, the license is considered breached.
+* Article 4. This license terminates automatically if the Licensee or a sublicensee breaches this license agreement. In case of a breach by the main Licensor, this entails termination of all sublicensees. A breach of the Licensing Terms obliges the Licensee to pay a fee to Licensor for Breach of the Licensing Terms.
+
+      </Text>
+      </Box>
+    </Col>
+    <Col sm={4}>
+      One of three columns
+    </Col>
+    <Col sm={4}>
+      One of three columns
+    </Col>
+  </Row>
+
+</Container>
         </div>)
     }
 }
